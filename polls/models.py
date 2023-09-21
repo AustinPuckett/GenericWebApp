27a8738be883f75e3e322dd0fb9c8001cfb9c4a3
Austin <puckettaustin@rocketmail.com>
2023-09-21 17:17:10 -0500

    Add Print Statement

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
+    print('Howdy')
     last_name = models.CharField(max_length=30)
 
     def __str__(self):
